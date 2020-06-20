993101686ee401aa6d1350a521fee34c35399e9a
HEINRICK7 <heinrickcostta@gmail.com>
2020-06-20 15:48:47 -0300

    delete

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import {getToken} from '../services/auth'
 
 const api = axios.create({
-    baseURL: 'http://localhost:3334',
+    baseURL: 'http://ec2-18-214-211-47.compute-1.amazonaws.com',
 });
 
 api.interceptors.request.use(async config =>{
