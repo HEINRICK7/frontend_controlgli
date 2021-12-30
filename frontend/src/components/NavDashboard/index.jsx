374924b77413e854b7f0fef9df116b274136ea5a
HEINRICK7 <heinrickcostta@gmail.com>
2021-12-30 16:06:18 -0300

    criando arquivos

---
+++ frontend/src/components/NavDashboard/index.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import { FiPower, FiUser} from 'react-icons/fi';
+import { Link, useHistory } from 'react-router-dom';
+
+import './index.css'
+
+
+const Nav = () => {
+
+    const history = useHistory();
+
+    const handleLogout = () => {
+        localStorage.clear();
+
+        history.push('/')
+    }
+
+    return (
+        <div className="nav_bar">
+            <header>
+                <span> 
+                    <FiUser color="FFF" size={20}/>  
+                     <p>{}</p>
+                     <Link className="dashboard" to="/profile"></Link>
+                </span>
+                
+                <button type="button" onClick={handleLogout}>
+                    <FiPower color="FFF" size={20}/> 
+                </button>
+               
+            </header> 
+        </div>
+    );
+}
+
+export default Nav;
